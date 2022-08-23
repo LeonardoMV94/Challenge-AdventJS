2c261280815db45244996ec9bc67fdb2cf33ed2a
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2022-08-23 19:59:52 -0400

    done day6

--- dia6.js
+++ dia6.js
@@ -20,10 +20,12 @@
  */
 
 function sumPairs(numbers, result) {
-    // ¡Y no olvides compartir tu solución en redes!
-    
-
-    return []
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = 1; j < numbers.length; j++) {            
+            if((i !=j && numbers[i]+numbers[j]) === result) return [numbers[i], numbers[j]]
+        }        
+    }
+    return null
 }
 console.log(sumPairs([3, 5, 7, 2], 10) )            // [3, 7]
 console.log(sumPairs([-3, -2, 7, -5], 10))          // null
